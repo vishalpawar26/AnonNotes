56cca9236f0bfddfd5cbbee3701a59df8b9d4930
vishalpawar26 <vishalpawar1784@gmail.com>
2024-07-07 20:28:47 +0530

    verification email using nodemailer

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,18 +1,55 @@
-import { resend } from "@/lib/resend";
-import VerificationEmail from "../../emails/verificationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
+import nodemailer from "nodemailer";
 
-export async function sendVerificationEmail(email: string, username: string, verificationCode: string): Promise<ApiResponse> {
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.APP_PASSWORD,
+  },
+});
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verificationCode: string
+): Promise<ApiResponse> {
+  const htmlCode: string = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+  <title>Verification Code</title>
+  </head>
+  <body>
+
+  <h2>Hello ${username},</h2>
+  <p>Thank you for registering. Please use the following verification code to complete your registration:</p>
+  <h3>${verificationCode}</h3>
+  <p>If you did not request this code, please ignore this email.</p>
+
+  </body>
+  </html>
+  `;
+
   try {
-    const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
+    // const { data, error } = await resend.emails.send({
+    //   from: 'Acme <onboarding@resend.dev>',
+    //   to: email,
+    //   subject: 'Mystery Message | Verification Code',
+    //   react: VerificationEmail({ username, otp: verificationCode }),
+    // });
+    await transporter.sendMail({
+      from: `"AnonNotes" <${process.env.SENDER_EMAIL}>`,
       to: email,
-      subject: 'Mystery Message | Verification Code',
-      react: VerificationEmail({ username, otp: verificationCode }),
+      subject: "AnonNotes | Verification Code",
+      html: htmlCode,
     });
-    return {success: true, message: "Verification email sent successfully"};
+    return { success: true, message: "Verification email sent successfully" };
   } catch (error) {
     console.error("Error while sending verification email", error);
-    return {success: false, message: "Failed to send verification email"};
+    return { success: false, message: "Failed to send verification email" };
   }
 }
